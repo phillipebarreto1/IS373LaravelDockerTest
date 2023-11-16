110354581ac9c9759f79bb368d9e4abf4797f049
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-16 14:42:59 -0500

    the delete button deletes the movie entry

--- laravel-app/resources/views/viewMovies.blade.php
+++ laravel-app/resources/views/viewMovies.blade.php
@@ -36,6 +36,7 @@
                             <td> {{$movie->title}}</td>
                             <td>{{$movie->yearReleased}}</td>
                             <td>{{$movie->avgRating}}</td>
+                            <td><button onclick="delete_movie({{$movie->id }})">Delete</button></td>
                         </tr>
                         @endforeach
 
@@ -62,7 +63,23 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 </body>
+<script>
+    function delete_movie(id) {
+        if (confirm("Want to delete?") === true) {
+            // call api to delete api/movie/{id}
+            axios.delete('/api/movie/' + id)
+                .then(function (response) {
+                    console.log("Delete movie");
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+</script>
 
 </html>
