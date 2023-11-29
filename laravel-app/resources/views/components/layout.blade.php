d04e47f8036e334904ce1a0ec83ecdd340fbde0f
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-29 11:20:16 -0500

    adjust layout of the page

--- laravel-app/resources/views/components/layout.blade.php
+++ laravel-app/resources/views/components/layout.blade.php
@@ -1,6 +1,8 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Todo Manager' }}</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -44,7 +46,7 @@
     <div class="container mt-5">
         {{ $slot }}
     </div>
-    
+
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
