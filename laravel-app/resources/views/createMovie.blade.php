bf2cd812503201c347a381a1558c630bbadfd79c
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-29 10:59:50 -0500

    define the layout for the website

--- laravel-app/resources/views/createMovie.blade.php
+++ laravel-app/resources/views/createMovie.blade.php
@@ -1,48 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Movie</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
+    <x-layout>
+        <div class="container mt-5">
+            <h1>Add Movie to List</h1>
 
-    <div class="container mt-5">
-        <h1>Add Movie to List</h1>
+            <!-- Movie Add Form -->
+            <form action="/api/movie" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="title">Title:</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Enter Movie Title"
+                        required>
+                </div>
 
-        <!-- Movie Add Form -->
-        <form action="/api/movie" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="title">Title:</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Enter Movie Title" required>
-            </div>
+                <div class="form-group">
+                    <label for="yearReleased">Year Released:</label>
+                    <input type="number" class="form-control" id="yearReleased" name="yearReleased"
+                        placeholder="Enter Year Released" required>
+                </div>
 
-            <div class="form-group">
-                <label for="yearReleased">Year Released:</label>
-                <input type="number" class="form-control" id="yearReleased" name="yearReleased" placeholder="Enter Year Released" required>
-            </div>
+                <div class="form-group">
+                    <label for="avgRating">Average Rating:</label>
+                    <input type="number" class="form-control" id="avgRating" name="avgRating"
+                        placeholder="Enter Average Rating" step="0.1" required>
+                </div>
 
-            <div class="form-group">
-                <label for="avgRating">Average Rating:</label>
-                <input type="number" class="form-control" id="avgRating" name="avgRating" placeholder="Enter Average Rating" step="0.1" required>
-            </div>
+                <!-- Add more fields as needed -->
 
-            <!-- Add more fields as needed -->
-
-            <button type="submit" class="btn btn-primary">Save</button>
-        </form>
-        <!-- End Movie Add Form -->
-
-    </div>
-
-    <!-- Bootstrap JS and dependencies (optional) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+                <button type="submit" class="btn btn-primary">Save</button>
+            </form>
+            <!-- End Movie Add Form -->
+        </div>
+    </x-layout>
 </body>
 </html>
--- laravel-app/resources/views/viewMovies.blade.php
+++ laravel-app/resources/views/viewMovies.blade.php
@@ -11,55 +11,51 @@
 </head>
 
 <body>
+    <x-layout>
+        <div class="container mt-5">
+            <h1>Movie List</h1>
+            <a href="/movie/create"><button type="button" class="btn btn-primary spacing my-4">Add</button></a>
+            <!-- End Box Links -->
+            <br>
+            <!-- Scrollable Box for Movie Entries -->
+            <div class="card mb-3">
+                <div class="card-body">
+                    <div class="overflow-auto" style="max-height: 300px;">
+                        <!-- Your movie entries will be added here dynamically -->
+                        <!-- Example entry: -->
+                        <p class="card-text">
+                        <table class="table">
+                            <tr>
+                                <th>Title</th>
+                                <th>Year Released</th>
+                                <th>Average Rating</th>
+                                <th></th>
+                            </tr>
 
-    <div class="container mt-5">
-        <h1>Movie List</h1>
-        <a href="/movie/create"><button type="button" class="btn btn-primary spacing my-4">Add</button></a>
-        <!-- End Box Links -->
-        <br>
-        <!-- Scrollable Box for Movie Entries -->
-        <div class="card mb-3">
-            <div class="card-body">
-                <div class="overflow-auto" style="max-height: 300px;">
-                    <!-- Your movie entries will be added here dynamically -->
-                    <!-- Example entry: -->
-                    <p class="card-text">
-                    <table class="table">
-                        <tr>
-                            <th>Title</th>
-                            <th>Year Released</th>
-                            <th>Average Rating</th>
-                            <th></th>
-                        </tr>
+                            @foreach ($data as $movie)
+                            <tr>
+                                <td> {{$movie->title}}</td>
+                                <td>{{$movie->yearReleased}}</td>
+                                <td>{{$movie->avgRating}}</td>
+                                <td>
+                                    <a class="px-2" href="/movie/info?id={{$movie->id }}"><button type="button"
+                                            class="btn btn-info">Info</button></a>
+                                    <a class="px-2" href="/movie/update?id={{$movie->id }}"><button type="button"
+                                            class="btn btn-primary">Update</button></a>
+                                    <button type="button" class="btn btn-danger" class="px-2"
+                                        onclick="delete_movie({{$movie->id }})">Delete</button>
+                                </td>
+                            </tr>
+                            @endforeach
 
-                        @foreach ($data as $movie)
-                        <tr>
-                            <td> {{$movie->title}}</td>
-                            <td>{{$movie->yearReleased}}</td>
-                            <td>{{$movie->avgRating}}</td>
-                            <td>
-                                <a class="px-2" href="/movie/info?id={{$movie->id }}"><button type="button" class="btn btn-info">Info</button></a>
-                                <a class="px-2" href="/movie/update?id={{$movie->id }}"><button type="button" class="btn btn-primary">Update</button></a>
-                                <button type="button" class="btn btn-danger" class="px-2" onclick="delete_movie({{$movie->id }})">Delete</button>
-                            </td>
-                        </tr>
-                        @endforeach
-
-                    </table>
-                    </p>
-                    <!-- Add more entries as needed -->
+                        </table>
+                        </p>
+                        <!-- Add more entries as needed -->
+                    </div>
                 </div>
             </div>
+            <!-- End Scrollable Box -->
         </div>
-        <!-- End Scrollable Box -->
-    </div>
-
-    <!-- Bootstrap JS and dependencies (optional) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-
 </body>
 <script>
     function delete_movie(id) {
@@ -76,6 +72,5 @@
         }
     }
 </script>
-
-
+</x-layout>
 </html>
