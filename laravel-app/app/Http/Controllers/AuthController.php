e8ed82f16f10ab1a24ec0b18dfa55fb7fa2e519a
Salmaan Saeed <salsaeed999@gmail.com>
2023-11-30 03:11:32 -0500

    got register api to work

--- laravel-app/app/Http/Controllers/AuthController.php
+++ laravel-app/app/Http/Controllers/AuthController.php
@@ -4,8 +4,42 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
     //
+    public function register(Request $request): string {
+        // CREATE
+        $email = $request->email;
+        $username = $request->username;
+        $password = $request->password;
+
+        Log::info('Help me');
+        Log::info($request->email);
+
+        $user = new User();
+
+        $email_record = User::where('email', '=', $email)->first();
+
+        if ($email_record != null) {
+            return "Email exists";
+        }
+
+        $username_record = User::where('username', '=', $username)->first();
+
+        if ($username_record != null) {
+            return "Username exists";
+        }
+
+        $user->email = $email;
+        $user->username = $username;
+        $user->password = Hash::make($password);
+
+        $user->save();
+
+        return redirect('/login');
+    }
 }
